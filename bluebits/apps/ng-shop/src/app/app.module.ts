4311fe0a8c1ebd2304db576798a71b33f25d3ca3
adeelkbw <adeelnasirkbw@gmail.com>
2022-08-02 03:46:00 +0500

    routing between components --client

--- bluebits/apps/ng-shop/src/app/app.module.ts
+++ bluebits/apps/ng-shop/src/app/app.module.ts
@@ -1,11 +1,19 @@
+import { ProductListComponent } from './pages/product-list/product-list.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+const routes: Routes = [
+    { path: '', component: HomePageComponent },
+    { path: 'product-list', component: ProductListComponent }
+];
+
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule],
+    imports: [BrowserModule, RouterModule.forRoot(routes)],
     providers: [],
     bootstrap: [AppComponent]
 })
